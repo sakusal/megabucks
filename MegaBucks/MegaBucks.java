a19b8bcaa580c3feba7cbf109220f2bb903cedc1
sakusal <35341575+sakusal@users.noreply.github.com>
2018-01-11 11:46:23 -0500

    Add files via upload

---
+++ MegaBucks/MegaBucks.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+/**
+ * Write a description of class MegaBucks here.
+ *
+ * @KhatiwadaSakusal & BhattShabit
+ * @version 1/11/2018
+ */
+public class MegaBucks
+{
+    // instance variables - replace the example below with your own
+    Scanner scan = new Scanner (System.in);
+
+    /**
+     * Constructor for objects of class MegaBucks
+     */
+    public MegaBucks()
+    {
+        // initialise instance variables
+        
+    }
+
+    
+}
---
+++ MegaBucks/MegaBucksTest.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+/**
+ * Write a description of class MegaBucksTest here.
+ *
+ * @KhatiwadaSakusal & BhattShabit)
+ * @version (a version number or a date)
+ */
+public class MegaBucksTest
+{
+    public static void main(String[] args)
+    {// instance variables - replace the example below with your own
+    Scanner scan = new Scanner(System.in);
+    ArrayList<Integer> userNumbers = new ArrayList<Integer>();
+    System.out.println("Enter your 6 numbers for the lottery");
+    final int numOfNums = 6;
+    for (int i=0;i<numOfNums;i++)
+    {
+        userNumbers[i]=scan.nextInt();
+    }
+    
+    
+
+    
+    
+
+    
+ }
+}
